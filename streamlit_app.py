7795ca5e0730d79d335325153f251c65ddc51e42
Emilio González Llamas <87701657+EGLl-99@users.noreply.github.com>
2023-06-18 15:28:51 -0600

    Commit
    Fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-##from streamlit_option_menu import option_menu
-
 import sys
 from streamlit.config import on_config_parsed
 from streamlit.web.cli import main
@@ -13,7 +11,7 @@ on_config_parsed(register_options_menu)
 sys.argv.extend(["streamlit_app.py", "C:/Users/egonz/Documents/PFOTV2/Version-final-MW-OT/streamlit_app.py"])
 main(prog_name="streamlit_app.py")
 
-
+from streamlit_option_menu import option_menu
 from apps import Transporte, prior, PoblaEcon, Geol, Cons, Clima # import your app modules here
 
 st.set_page_config(page_title= 'Ordenamiento Territorial',page_icon=':tada:',layout='wide')
